8146086202e34cad07dc70db1c3140dbb3e48870
javo2023 <jguzmanalarcon@gmail.com>
2023-06-28 22:45:08 -0400

    terminado falta imagen

--- app/src/main/java/com/example/ejercicio_12/MenuActivity.java
+++ app/src/main/java/com/example/ejercicio_12/MenuActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.example.ejercicio_12.databinding.ActivityMainBinding;
 import com.example.ejercicio_12.databinding.ActivityMenuBinding;
@@ -21,23 +22,26 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMenuBinding binding = ActivityMenuBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         List<String>listaDeMenus = new ArrayList<>();
 
         listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
-        listaDeMenus.add("Pure de calabacin");
+        listaDeMenus.add("Pure de papas");
+        listaDeMenus.add("Cazuela");
+        listaDeMenus.add("Pastel de choclos");
+        listaDeMenus.add("Ensaladas surtidas");
+        listaDeMenus.add("Carbonada");
+        listaDeMenus.add("Tallarines");
+        listaDeMenus.add("Arroz con pollo");
+        listaDeMenus.add("Bistec a lo pobre");
+        listaDeMenus.add("Budin de zapallos");
+        listaDeMenus.add("Salmon con agregado");
+
+        ArrayAdapter adapter = new ArrayAdapter (this,android.R.layout.simple_list_item_1,listaDeMenus);
+        binding.ListViewMenu.setAdapter(adapter);
 
 
 
